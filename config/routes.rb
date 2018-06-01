069621f6bfa6bb78835da8d0f2b8f271edc36532
tanvir hasan <tanvir002700@gmail.com>
2018-06-02 03:04:00 +0600

    create api version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
-  post 'sign_up', to: 'registrations#create'
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      post 'sign_up', to: 'registrations#create'
+    end
+  end
 end
