f1719efb76f80fd0c7671fb1d54fcf6815aff4ac
tanvir hasan <tanvir002700@gmail.com>
2018-06-02 11:39:10 +0600

    add post controller

--- README.md
+++ README.md
@@ -22,3 +22,16 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+api call
+
+curl -d '{"email": "test@test", "password": "1234567"}' \
+     -H "Content-Type: application/json" \
+     -X POST \
+     http://localhost:3000/api/v1/sign_in/
+
+
+curl -d '{"name": "test", "email": "test@test.com", "password": "1234567", "password_confirmation": "1234567"}' \
+     -H "Content-Type: application/json" \
+     -X POST \
+   http://localhost:3000/api/v1/sign_up/
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_01_123812) do
+ActiveRecord::Schema.define(version: 2018_06_02_053748) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
