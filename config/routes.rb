48e05e5444f232f698291e924cbe00aeacb3c86c
tanvir hasan <tanvir002700@gmail.com>
2018-06-02 12:32:54 +0600

    change post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       post 'sign_up', to: 'registrations#create'
       post 'sign_in', to: 'seasons#create'
+      resources :posts
     end
   end
 end
